e9283142392ca3c8a8fa12a986282ea05a397d54
Valentyn Mostovyi <mostovoy.v@gmail.com>
2025-01-24 15:42:12 +0100

    Change Welcome

--- src/server.js
+++ src/server.js
@@ -37,7 +37,7 @@ export const setupServer = () => {
 
   app.get('/', (req, res) => {
     res.json({
-      message: 'Wellcome to Contacts App!',
+      message: 'Welcome to Contacts App!',
     });
   });
 
